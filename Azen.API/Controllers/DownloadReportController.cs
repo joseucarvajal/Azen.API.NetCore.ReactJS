14fc4258a114c80ef911575da83c90093a0e1cc3
cahpineda <cahpineda@gmail.com>
2020-08-27 17:16:02 -0500

    Create DownloadReportControler, Folder temp

---
+++ Azen.API/Controllers/DownloadReportController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Azen.API.Controllers
+{
+    public class DownloadReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            Response.ContentType = "text/plain";
+            Response.AddHeader("Content-Disposition", "attachment; filename=file.txt");
+            Response.TransmitFile(Server.MapPath("file.txt"));
+            return View();
+        }
+    }
+}
