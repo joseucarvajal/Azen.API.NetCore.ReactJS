1a53bd2c54944fec61ddd1f2b67729a72f073982
joseucarvajal <joseucarvajal@gmail.com>
2020-08-27 15:16:03 -0500

    Code formatting

--- Azen.API/Controllers/CommandController.cs
+++ Azen.API/Controllers/CommandController.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Azen.API.Sockets.Auth;
+using Azen.API.Sockets.Comunications;
+using Azen.API.Sockets.Helpers;
 using Azen.API.Sockets.Settings;
 using Azen.API.Sockets.Utils;
-using Azen.API.Sockets.Comunications;
-using Azen.API.Sockets.General;
-using Azen.API.Sockets.Domain;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json.Linq;
-using MediatR;
-using Azen.API.Sockets.Helpers;
 using Newtonsoft.Json;
-using Azen.API.Sockets.Auth;
-using Microsoft.AspNetCore.Cors;
+using System.Threading.Tasks;
+using Azen.API.Model.ZCommand;
 
 namespace Azen.API.Controllers
 {
@@ -28,17 +22,20 @@ namespace Azen.API.Controllers
         private readonly IOptions<AzenSettings> _azenSettings;
         private readonly IMediator _mediator;
 
-        public CommandController(IOptions<AzenSettings> azenSettings, ZSocket zSocket, LogHandler logHandler, IMediator mediator)
+        public CommandController(
+            IOptions<AzenSettings> azenSettings, 
+            ZSocket zSocket, 
+            LogHandler logHandler, 
+            IMediator mediator)
         {
             _azenSettings = azenSettings;
             _logHandler = logHandler;
             _zsck = zSocket;
             _mediator = mediator;
         }
-
-        //[EnableCors]
+        
         [HttpPost("aceptarlogin")]
-        public async Task<ActionResult<string>> AceptarLogin(Model.ZCommand.Execute.Command command)
+        public async Task<ActionResult<string>> AceptarLogin(Execute.Command command)
         {
             string response = await _mediator.Send(command);
             return response;
@@ -46,7 +43,9 @@ namespace Azen.API.Controllers
         
         [HttpPost("{idaplicacion}/execute")]
         [Authorize]
-        public async Task<ActionResult<string>> Execute([FromRoute] string idAplicacion, [FromBody] Model.ZCommand.Execute.Command command)
+        public async Task<ActionResult<string>> Execute(
+            [FromRoute] string idAplicacion, 
+            [FromBody] Execute.Command command)
         {
             string zClaimsStr = HttpContext.Items["ZClaims"] as string;
 
