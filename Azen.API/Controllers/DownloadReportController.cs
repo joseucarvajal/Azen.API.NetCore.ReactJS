4c34bf11acddb23dcf799d2ebd0dbd317e28f6ad
cahpineda <cahpineda@gmail.com>
2020-10-02 07:55:41 -0500

    include log Downloadreport

--- Azen.API/Controllers/DownloadReportController.cs
+++ Azen.API/Controllers/DownloadReportController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Azen.API.Sockets.Helpers;
+using Azen.API.Sockets.Utils;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
@@ -11,14 +12,28 @@ using Microsoft.AspNetCore.StaticFiles;
 namespace Azen.API.Controllers
 {
 	public class DownloadReportController : Controller
-	{		
-		[HttpGet("downloadreport/{filename}")]
-		public async Task<IActionResult> Index(string fileName)
+	{
+		LogHandler _logHandler;
+
+		public DownloadReportController(LogHandler logHandler)
+		{
+			_logHandler = logHandler;
+		}
+
+		[HttpGet("downloadreport/{filename}/{log?}")]
+		public async Task<IActionResult> Index(
+			[FromRoute] string fileName,
+			[FromRoute] int? log)
 		{
 			string[] paths = { @"tmp", fileName };
 			string fullPath = Path.Combine(paths);
 
 			byte[] data = await System.IO.File.ReadAllBytesAsync (fullPath);
+
+			if ((log ?? 0) != 0)
+			{
+				_logHandler.Debug($"File: {fileName}, Length: {data.Length}");
+			}
 
 			string contentType;
 			var provider = new FileExtensionContentTypeProvider();
