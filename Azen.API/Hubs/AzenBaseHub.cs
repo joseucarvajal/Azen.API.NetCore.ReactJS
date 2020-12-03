36174764ad3fe4e2b03da67aa18ff210aa9a2723
Carlos Hurtado <cahpineda@gmail.com>
2020-12-02 19:19:31 -0500

    xxx

---
+++ Azen.API/Hubs/AzenBaseHub.cs
@@ -0,0 +1,21 @@
+﻿using Azen.API.Sockets.Auth;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azen.API.Hubs
+{
+    public class AzenBaseHub : Hub
+    {
+        protected ZClaims GetZClaims()
+        {
+            //Todo:
+            //string zClaimsStr = HttpContext.Items["ZClaims"] as string;
+            string zClaimsStr = string.Empty;
+            return JsonConvert.DeserializeObject<ZClaims>(zClaimsStr);
+        }
+    }
+}
