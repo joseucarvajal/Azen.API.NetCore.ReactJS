eef680cf4726badaecc8f49f8d83a8bd23e016a8
Carlos Hurtado <cahpineda@gmail.com>
2020-12-28 18:45:11 -0500

    Parameter enc to send buffer

--- Azen.API/Controllers/CommandController.cs
+++ Azen.API/Controllers/CommandController.cs
@@ -1,5 +1,6 @@
 ﻿using Azen.API.Models.ZCommand;
 using Azen.API.Sockets.Comunications;
+using Azen.API.Sockets.Cryptography;
 using Azen.API.Sockets.Helpers;
 using Azen.API.Sockets.Settings;
 using Azen.API.Sockets.Utils;
@@ -16,34 +17,48 @@ namespace Azen.API.Controllers
         ZSocket _zsck;
         private readonly IOptions<AzenSettings> _azenSettings;
         private readonly IMediator _mediator;
+        private ZCryptography _zCryptography;
 
         public CommandController(
             IOptions<AzenSettings> azenSettings, 
             ZSocket zSocket, 
             LogHandler logHandler, 
-            IMediator mediator)
+            IMediator mediator,
+            ZCryptography zCryptography)
         {
             _azenSettings = azenSettings;
             _logHandler = logHandler;
             _zsck = zSocket;
             _mediator = mediator;
+            _zCryptography = zCryptography;
         }
         
-        [HttpPost("aceptarlogin")]
-        public async Task<ActionResult<string>> AceptarLogin(Execute.Command command)
+        [HttpPost("aceptarlogin/{enc?}")]
+        public async Task<ActionResult<string>> AceptarLogin(Execute.Command command,
+            [FromRoute] int? enc)
         {
+            if (enc != null)
+            {
+                command.Buffer = _zCryptography.GetCipherText(command.Buffer);
+            }
             command.RemoteIpAddress = Request.HttpContext.Connection.RemoteIpAddress;
 
             string response = await _mediator.Send(command);
             return response;
         }
         
-        [HttpPost("{idaplicacion}/execute")]
+        [HttpPost("{idaplicacion}/execute/{enc?}")]
         [Authorize]
         public async Task<ActionResult<string>> Execute(
-            [FromRoute] string idAplicacion, 
+            [FromRoute] string idAplicacion,
+            [FromRoute] int? enc,
             [FromBody] Execute.Command command)
         {
+            if (enc != null)
+            {
+                command.Buffer = _zCryptography.GetCipherText(command.Buffer);
+            }
+
             var zClaims = GetZClaims();
 
             command.IdAplication = idAplicacion;
